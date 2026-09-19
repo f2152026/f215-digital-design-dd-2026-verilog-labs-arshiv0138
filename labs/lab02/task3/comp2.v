62bfada3917991f1a9b00fb6449eef733c524c48
Arshiv0138 <rajarshivbbr@gmail.com>
2026-09-19 12:49:15 +0000

    lab02 task3

--- labs/lab02/task3/comp2.v
+++ labs/lab02/task3/comp2.v
@@ -1,12 +1,9 @@
 // comp2.v
 // 2-bit unsigned magnitude comparator.
-// Given two 2-bit values A and B, exactly one of GT, LT, EQ should be 1
+// Given two 2-bit values A and B, exactly one of GT, LT, EQ is 1
 // for any input combination.
-//
-// This module has a bug that a *self-checking* testbench should catch on
-// its own -- you should not need to inspect the code below to find it.
-// Write your testbench first, let it tell you something is wrong, THEN
-// come back and fix this file.
+
+`timescale 1ns / 1ps
 
 module comp2 (
   input  [1:0] A,
@@ -17,7 +14,7 @@ module comp2 (
 );
 
   assign EQ = (A == B);
-  assign GT = (A >= B);
+  assign GT = (A >  B);   // BUG FIX: was >=, which made GT and EQ both high when A == B
   assign LT = (A <  B);
 
 endmodule
